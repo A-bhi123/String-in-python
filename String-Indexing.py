bd4410bb9264333f6afddee07345669b6147bb66
A-bhi123 <abhishekkumarbind616@gmail.com>
2026-06-09 17:22:57 +0530

    Include the negative indexing

--- String-Indexing.py
+++ String-Indexing.py
@@ -6,4 +6,11 @@ print(text[5])
 
 print(paragraph[13])
 print(paragraph[14])
-print(paragraph[15])
+print(paragraph[15])   
+
+# Negative indexing 
+
+text = "Lucknow"
+
+print(text[-1])
+print(text[-2])
