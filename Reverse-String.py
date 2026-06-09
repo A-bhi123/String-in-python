f189361dc36cd85c2e078aa945d0f735f6be8d01
A-bhi123 <abhishekkumarbind616@gmail.com>
2026-06-09 18:49:16 +0530

    Write the code of Reverse the charector and words, convert the string into list and list to string

---
+++ Reverse-String.py
@@ -0,0 +1,14 @@
+text = "Python is too easy"
+
+# Reverse the charector
+print(text[::-1])
+
+# Reverse the word
+print(" ".join(text.split()[::-1]))
+
+# Convert the string into list
+print(text.split())
+
+# conver the list into string
+print(" ".join(text.split()))
+
